28c50e840f24f339f986ff767acca9f54fde253d
Dungsten <sagarchaurasia462@gmail.com>
2021-07-06 13:43:34 +0530

    added cards component

---
+++ src/Components/Cards/Card/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Card.css"
+
+function Card() {
+    return (
+        <>
+            <div className="cards-component">
+                <h2 className="component-heading">diagonal sum</h2>
+                <p>C++</p>
+            </div>
+        </>
+    )
+}
+
+export default Card
--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,12 +1,42 @@
 import React from 'react'
 import Navbar from '../Navbar'
+import "./Cards.css"
+import Card from './Card/Card'
+import { IoMdCreate } from 'react-icons/io';
 
 function Cards() {
     return (
         <>
-
+            <div>
+                <button className="train-btn">
+                    Train Now
+                </button>
+            </div>
             <div className="container-cards">
-
+                <div className="d1">
+                    <div className="create-card">
+                        <h4>Create new card</h4>
+                    </div>
+                    <div className="cardholder">
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                    </div>
+                </div>
+                <div className="d2">
+                    <div className="make-function">
+                        <h1>make a function</h1>
+                        <div className="language">c++</div>
+                    </div>
+                    <div className="card-btn-container">
+                        <button className="card-btn"><IoMdCreate />Edit</button>
+                        <button className="card-btn">Delete</button>
+                    </div>
+                </div>
 
             </div>
         </>
