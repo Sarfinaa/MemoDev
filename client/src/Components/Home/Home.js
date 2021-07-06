dfd114d263b37a0829770807c4a1fccdf46ba03d
Sarfinaa <karanseth200066@gmail.com>
2021-07-06 23:15:08 +0530

    added getPost

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React,{useEffect} from "react";
 import "./home.css";
 import HeroSection from "../HeroSection";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import { getCards } from '../../actions/cards';
+import {  useDispatch} from "react-redux";
 export default function () {
+   const dispatch = useDispatch();
+  useEffect(()=>{
+dispatch(getCards());
+  },[dispatch])
   return (
     <div>
       <Router>
--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -1,3 +1,4 @@
 export const AUTH='AUTH';
 export const LOGOUT='LOGOUT';
 export const CREATE= 'CREATE';
+export const FETCH_ALL='FETCH_ALL';
