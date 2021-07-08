5973c0943a6d34c622cbe4aa5cbb7879a9e2a17c
Dungsten <sagarchaurasia462@gmail.com>
2021-07-09 01:05:52 +0530

    button

--- client/src/Components/Training/Training.js
+++ client/src/Components/Training/Training.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Training.css";
-
+import Editor from '../Editor/Editor'
 function Training() {
   return (
     <div>
@@ -11,11 +11,15 @@ function Training() {
               <p className="t-card-language">C++</p>
           </div>
           <div className="tr2">
-              
+              <Editor/>
+          </div>
+          <div className="tr2.5">
+          <button className='btn-tr3'>Good</button>
+          <button className='btn-tr3'>Give another shot</button>
+          
           </div>
           <div className="tr3">
             <button className='btn-tr3'>Submit</button>
-
           </div>
         </div>
       </div>
